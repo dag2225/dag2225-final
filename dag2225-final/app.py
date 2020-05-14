126b680fd5a96f26e64738d0e3eeb0b74728d19e
dag2225 <65304967+dag2225@users.noreply.github.com>
2020-05-14 13:30:07 -0400

    Added code documentation

--- dag2225-final/app.py
+++ dag2225-final/app.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Apr 21 14:57:17 2020
-
 @author: Devin Golla
+UNI: dag2225
+Overview of Program: Utilizes Flask library to set up routes for navigation between pages
 """
 
 #import statements
@@ -14,15 +14,24 @@ app = Flask(__name__)
 #static route
 @app.route("/") 
 # if you go to this home route, then it will show whatever you define this function to you
-def hello():
+def index():
+    """
+    Specifies action of home page route
+    """
     return render_template("index.html")
 
 @app.route("/assignments.html") 
-def hello1():
+def assignments():
+    """
+    Specifies action of assignment page route
+    """
     return render_template("/assignments.html")
 
 @app.route("/classes.html") 
-def hello2():
+def classes():
+    """
+    Specifies action of classes page route
+    """
     return render_template("/classes.html")
 
 #start the server
---
+++ dag2225-final/readme.txt
@@ -0,0 +1,17 @@
+Name: Devin Golla
+UNI: dag2225
+Homework: Final Project
+Brief summary of work:
+
+app.py: Utilizes Flask library to set up routes for navigation between pages (index() controls render of index.html, assignments() controls render of assignments.html, classes() controls render of classes.html)
+
+index.html: Specifies formatting and layout of home page
+
+assignments.html: Specifies formatting and layout of assignments page
+
+classes.html: Specifies formatting and layout of classes page
+
+Image citations:
+Amazon.com. “Scrubs Season 2 DVD.” Wikipedia, n.d., https://en.wikipedia.org/wiki/File:Scrubs-s2-dvd.jpg
+Biagini, Jack. “Mitochondria is the Powerhouse of the Cell iPad Case & Skin.” Redbubble, 2020, https://www.redbubble.com/i/ipad-case/Mitochondria-is-the-Powerhouse-of-the-Cell-by-JackBiagini/15469360.MNKGF
+All other images are my own
